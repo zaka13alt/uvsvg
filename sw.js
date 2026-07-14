9c734ec51c30d5c05f2eb8c41caabbf5eb431041
zaka13alt <zakariyaliban719@gmail.com>
2026-07-14 10:47:09 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,20 @@
-importScripts('./uv/uv.sw.js');
+// 
+const cdn1 = "https://cdn.jsdelivr.net/";
+const cdn2 = "gh/zaka13alt/";
+const cdn3 = "uvsvg@main/";
+const cdn4 = "uv/";
 
+// 
+const bundleUrl = cdn1 + cdn2 + cdn3 + cdn4 + "uv.bundle.js";
+const configUrl = cdn1 + cdn2 + cdn3 + cdn4 + "uv.config.js";
+const swCoreUrl = cdn1 + cdn2 + cdn3 + cdn4 + "uv.sw.js";
+
+// 
+importScripts(bundleUrl);
+importScripts(configUrl);
+importScripts(swCoreUrl);
+
+// 
 const sw = new UVServiceWorker();
 
 self.addEventListener('fetch', event =>
